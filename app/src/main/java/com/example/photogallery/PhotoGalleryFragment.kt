9894002766a522f32ec32a5dafcf6a1e681e2479
vagabondms <hiseokseok@gmail.com>
2023-04-13 22:03:57 +0900

    first commit

---
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -0,0 +1,36 @@
+package com.example.photogallery
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class PhotoGalleryFragment : Fragment() {
+
+    private lateinit var photoRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
+
+        photoRecyclerView = view.findViewById(R.id.photo_recycle_view)
+        photoRecyclerView.layoutManager = GridLayoutManager(context,3)
+
+        return view
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = PhotoGalleryFragment()
+    }
+}
