77f0a6d1117191e0cf8ee0bea67de137a0089500
kimms <kimms245@brandi.co.kr>
2023-04-14 17:10:25 +0900

    handle view lifecycle

--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -1,27 +1,21 @@
 package com.example.photogallery
 
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.provider.ContactsContract.Contacts.Photo
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
-import androidx.constraintlayout.widget.Placeholder
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
 private const val TAG = "PhotoGalleryFragment"
 
@@ -29,7 +23,7 @@ class PhotoGalleryFragment : Fragment() {
 
     private lateinit var photoRecyclerView: RecyclerView
     private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
-    private lateinit var thumbnailDownloader: ThumbnailDownloader<Placeholder>
+    private lateinit var thumbnailDownloader: ThumbnailDownloader<PhotoHolder>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,8 +32,13 @@ class PhotoGalleryFragment : Fragment() {
 
         photoGalleryViewModel = ViewModelProvider(this).get(PhotoGalleryViewModel::class.java)
 
-        thumbnailDownloader = ThumbnailDownloader()
-        lifecycle.addObserver(thumbnailDownloader)
+        val responseHandler = Handler(Looper.getMainLooper())
+        thumbnailDownloader = ThumbnailDownloader(responseHandler) { photoHolder, bitmap ->
+            val drawable = BitmapDrawable(resources, bitmap)
+            photoHolder.bindDrawable(drawable)
+        }
+
+        lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleEventObserver)
 
     }
 
@@ -53,6 +52,8 @@ class PhotoGalleryFragment : Fragment() {
         photoRecyclerView = view.findViewById(R.id.photo_recycle_view)
         photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
 
+        viewLifecycleOwner.lifecycle.addObserver(thumbnailDownloader.viewLifecycleEventObserver)
+
         return view
     }
 
@@ -65,9 +66,14 @@ class PhotoGalleryFragment : Fragment() {
         }
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewLifecycleOwner.lifecycle.removeObserver(thumbnailDownloader.viewLifecycleEventObserver)
+    }
+
     override fun onDestroy() {
         super.onDestroy()
-        lifecycle.removeObserver(thumbnailDownloader)
+        lifecycle.removeObserver(thumbnailDownloader.fragmentLifecycleEventObserver)
     }
 
     private class PhotoHolder(itemView: ImageView) : RecyclerView.ViewHolder(itemView) {
@@ -85,10 +91,12 @@ class PhotoGalleryFragment : Fragment() {
         override fun getItemCount(): Int = galleryItems.size
 
         override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
+            val galleryItem = galleryItems[position]
             val placeholder: Drawable =
                 ContextCompat.getDrawable(requireContext(), R.drawable.bill_up_close)
                     ?: ColorDrawable()
             holder.bindDrawable(placeholder)
+            thumbnailDownloader.queueThumbnail(holder, galleryItem.url)
 //            holder.bind(galleryItems[position])
         }
     }
--- app/src/main/java/com/example/photogallery/ThumbnailDownloader.kt
+++ app/src/main/java/com/example/photogallery/ThumbnailDownloader.kt
@@ -1,31 +1,93 @@
 package com.example.photogallery
 
+import android.graphics.Bitmap
+import android.os.Handler
 import android.os.HandlerThread
+import android.os.Message
 import android.util.Log
-import androidx.lifecycle.*
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.collections.set
 
 private const val TAG = "ThumbnailDownloader"
+private const val MESSAGE_DOWNLOAD = 0
 
 // in keyword: Should only be consumed. not returned
-class ThumbnailDownloader<in T> : HandlerThread(TAG), LifecycleEventObserver {
+class ThumbnailDownloader<in T>(
+    private val responseHandler: Handler,
+    private  val onThumbnailDownloaded : (T, Bitmap) -> Unit
+) : HandlerThread(TAG) {
 
     private var hasQuit = false
+    private lateinit var requestHandler: Handler
+    private val requestMap = ConcurrentHashMap<T,String>()
+    private val flickrRepository = FlickrRepository()
 
     override fun quit(): Boolean {
         hasQuit = true
         return super.quit()
     }
 
-    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-        when(event){
-            Lifecycle.Event.ON_CREATE -> Log.i(TAG, "Starting background thread")
-            Lifecycle.Event.ON_DESTROY -> Log.i(TAG, "Destroying background thread")
-            else -> Unit
+    val fragmentLifecycleEventObserver : LifecycleEventObserver =
+        LifecycleEventObserver { source, event ->
+            when (event) {
+                Lifecycle.Event.ON_CREATE -> {
+                    Log.i(TAG, "Starting background thread")
+                    start()
+                    looper
+                }
+                Lifecycle.Event.ON_DESTROY -> {
+                    Log.i(TAG, "Destroying background thread")
+                    quit()
+                }
+                else -> Unit
+            }
+        }
+
+    val viewLifecycleEventObserver : LifecycleEventObserver =
+        LifecycleEventObserver { source, event ->
+            when(event){
+                Lifecycle.Event.ON_DESTROY ->{
+                    Log.i(TAG, "Clearing all requests from queue")
+                    requestHandler.removeMessages(MESSAGE_DOWNLOAD)
+                    requestMap.clear()
+                }
+                else -> Unit
+            }
+        }
+
+    override fun onLooperPrepared() {
+        requestHandler = object : Handler(looper){
+            override fun handleMessage(msg: Message) {
+                if(msg.what == MESSAGE_DOWNLOAD){
+                    val target = msg.obj as T
+                    Log.i(TAG, "Got a request for URL: ${requestMap[target]}")
+                    handleRequest(target)
+                }
+            }
         }
     }
 
-    fun queueThumbnail(target: T, url: String){
+    private fun handleRequest(target: T) {
+        val url = requestMap[target] ?: return
+        val bitmap = flickrRepository.fetchPhoto(url) ?: return
+
+        responseHandler.post(Runnable {
+            if(requestMap[target] != null || hasQuit){
+                return@Runnable
+            }
+            requestMap.remove(target)
+            onThumbnailDownloaded(target,bitmap)
+        })
+    }
+
+
+    fun queueThumbnail(target: T, url: String) {
         Log.i(TAG, "Got a URL: $url")
+        requestMap[target] = url
+        requestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget()
     }
 
 }
